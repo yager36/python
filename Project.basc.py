7339e49f15d2ea4dc8c31858ba1888c6f54bbfc9
yager36 <44434281+yager36@users.noreply.github.com>
2018-11-22 18:58:26 +0200

    Add files via upload

--- Project.basc.py
+++ Project.basc.py
@@ -1,23 +1,28 @@
 
-def Check_slot(board,x):
+import random
+
+obj={                                # object of counting
+  "cut":0,
+  "cut1":0,
+  "cut_equal":0
+}                                    
+def print_win():                     # function which declares the number of victories
+    print("Number of victories first player:",obj["cut"])
+    print("Number of victories second player:",obj["cut1"])
+def Check_slot(board,x):            
     x=int(x)
     if board[x-1]=="x" or board[x-1]=="o":
         return 1
     else:
         return 0    
-
-
-def fun_chack(a):
+def fun_chack(a):                    # function checks if the conditions, only number and no mor than 1 digit
     if len(a)>1 or len(a)<1:
-        return 1  
-         
+        return 1          
     if  ord(a) > 48 and  ord(a) <58:
         return 0
     else:
         return 1    
-
-
-def print_board(board):   
+def print_board(board):              # function print the board game
     cut=1
 
     print('-----------------')
@@ -30,13 +35,11 @@ def print_board(board):
             print()
             print('-----------------')
             cut=1   
-          
-
-def rule(board):
+def rule(board):                    # function that checks the game moves "tic-tac-toe" and cheking the  winner
 
     for x in range(0,9):
         for y in range(0,9):
-            for r in range(0,9):              
+            for r in range(0,9):         
                 if (x==0 and y==1 and r==2)  or (x==3 and y==4 and r==5) or (x==6 and y==7 and r==8):
                     if  board[x]=="x" and board[y]=="x" and board[r]=="x":
                         return 1
@@ -48,98 +51,111 @@ def rule(board):
                         return 1
                     elif board[x]=="o" and board[y]=="o" and board[r]=="o":
                         return 2     
-
-                #print(x,y,r)
+               
                 if (x==0 and y==4 and r==8)  or (x==2 and y==4 and r==6):
 
                     if  board[x]=="x" and board[y]=="x" and board[r]=="x":
                         return 1
                     elif board[x]=="o" and board[y]=="o" and board[r]=="o":
-                        return 2    
+                        return 2
+         
+def equal(board):       # function that checks the result of equality
+    for x in range(0,9):        
+        if  board[x]=="x" or board[x]=="o":
+            obj["cut_equal"]=obj["cut_equal"]+1
 
+        if  obj["cut_equal"]==9:
+            return 3
 
-
-
-
-def start_to_play(x,board,fp):
-    x=int(x)
-
+def start_to_play(x,board,fp):  # function enters str,"x", or "o" to the game board/summed up the number of victories/Activates the functions, equal
+    x=int(x)                    # /lets_play_again/ print_board
     if  'first player'== fp:
         board[x-1]="x"
     elif 'second player'==sp:
-        board[x-1]="o"
-           
-def chack_game(a):
+        board[x-1]="o"         
+def chack_game(board,p):
+    obj["cut_equal"]=0
     if  rule(board)==1:
         print('first player won')  
-        a=input("for mor game press y else press n  ")
-
+        print_board(board)
+        p=lets_play_again(p)
+        obj["cut"]=obj["cut"]+1
     elif rule(board)==2:
-        print('second player won')  
-        a=input("for mor game press y else press n  ")  
+        print('second player won')
+        print_board(board)
+        obj["cut1"]=obj["cut1"]+1
+        p=lets_play_again(p)
+    elif equal(board)==3: 
+        print('A draw between two players')
+        print_board(board)
+        p=lets_play_again(p)
+    else:
+        obj["cut_equal"]=0      
+    return p    
+def lets_play_again(p): #At the end of the game the function asks the user to "play again or end a game
+    i=1
+    p=input("For play again press 'y' for exit press 'n':\n")
+    while(p!='y' and p!='n'):
+        p=input("worng input please try again For play again press 'y' for exit press 'n'")
+    return p
 
-
-  
-    return a                 
-
+    
 print("Welcome to x and o game")
 fp='first player'
 sp='second player'
-
 board=[1,2,3,4,5,6,7,8,9]
+lulaa=100
+p=" " 
+cup=input("to play with agains compuer press 1\nto play 2 player press 2:  ")
 print_board(board)
-a='y'
-while a=='y':
-    x=input('choose please location you want to mark first player:\n')
-     
+
+while lulaa: # start the game loop
+
+    x=input('choose please location you want to mark first player:\n')   
     while  fun_chack(x):
         x=input('wrong input choose please location you want to mark first player:\n')
-
     while Check_slot(board,x):
         x=input('Occupancy slot try again\n')
-
     start_to_play(x,board,fp)
-    print_board(board)
-    chack_game(a)   
-  
-    o=input('choose please location you want to mark second player:\n')
+    p=chack_game(board,p) 
+    print_board(board) 
+    if p=='n':
+        print_win()
+        break
+    elif p=='y':
+        board=[1,2,3,4,5,6,7,8,9]
+        print_board(board)
+        p=" " 
+        print_win()
+        continue
+    if cup=='1':
+        o=str(random.randint(0, 8))
+    elif cup=='2':
+        o=input('choose please location you want to mark second player:\n')    
     while  fun_chack(o):
-        o=input('wrong input choose please location you want to mark first player:\n')
+        if cup=='1':
+            o=str(random.randint(0, 8))
+        elif cup=='2':    
+             o=input('wrong input choose please location you want to mark first player:\n')
     while Check_slot(board,o):
-        o=input('Occupancy slot try again\n')    
-        
-    start_to_play(o,board,sp)
+        if cup=='1':
+            o=str(random.randint(0, 8))    
+        elif cup=='2':      
+            o=input('Occupancy slot try again\n')   
+    start_to_play(o,board,sp) 
     print_board(board)
-    a=chack_game(a)
-
-    
-else:
-    print ("winer")
-   
-    
-
-
-  
-
-       
+    p=chack_game(board,p)
+    if p=='n':
+        print_win()
+        break
+    elif p=='y':
+        board=[1,8,3,6,5,9,7,2,9] #Reset the array
+        print_board(board)
+        p=" " 
+        print_win()
+        continue 
 
 
-   
-    
-
-
-
-           
-              
-
-            
-   
-       
-      
-    
-
-
-
-
+        
 
 
